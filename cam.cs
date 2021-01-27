c79c34e7b545b99d2a6c068c07215a8009511dcd
Cesar Sodre <47694435+csodre@users.noreply.github.com>
2021-01-26 22:04:43 -0300

    Add files via upload

---
+++ cam.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam : MonoBehaviour
+{
+    public Transform corpo;
+    public Transform cabeca;
+
+    float rotationX = 0;
+    float rotationY = 0;
+    float angleYmin = -45.0f;
+    float angleYmax = 45.0f;
+    float mouseSens = 2.5f;
+    float suavidadeCam = 0.1f;
+
+    float suavidadeRotX=0;
+    float suavidadeRotY=0;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = cabeca.position;
+    }
+
+    void Update()
+    {
+        float moveVertival = Input.GetAxis("Mouse Y")*mouseSens;
+        float moveHorizontal = Input.GetAxis("Mouse X")*mouseSens;
+
+        suavidadeRotX = Mathf.Lerp(suavidadeRotX,moveHorizontal,suavidadeCam);
+        suavidadeRotY = Mathf.Lerp(suavidadeRotY,moveVertival,suavidadeCam);
+
+
+
+        rotationX += suavidadeRotX;
+        rotationY += suavidadeRotY;
+
+        rotationY = Mathf.Clamp(rotationY,angleYmin,angleYmax);
+
+        corpo.localEulerAngles = new Vector3(0,rotationX,0);
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+    }
+}
---
+++ move.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 eixoY;
+    Vector3 eixoZ;
+    Vector3 eixoX;
+    float velMove =5.0f;
+
+    float gravidade,velPulo,altMaxPulo,tempoAltPulo;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        altMaxPulo = 2.0f;
+        tempoAltPulo = 0.5f;
+        gravidade = (-2*altMaxPulo)/(tempoAltPulo*tempoAltPulo);
+        velPulo = (2*altMaxPulo)/tempoAltPulo;
+
+    }
+
+    void Update()
+    {
+        float moveX= Input.GetAxis("Horizontal");
+        float moveZ= Input.GetAxis("Vertical");
+
+        eixoX = moveX * velMove * transform.right;
+        eixoZ = moveZ * velMove * transform.forward;
+        eixoY += gravidade * Time.deltaTime * Vector3.up;
+
+
+
+        Vector3 velFinal = eixoY+eixoX+eixoZ;
+        controller.Move(velFinal*Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            eixoY = Vector3.down;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)&&controller.isGrounded)
+        {
+            eixoY = velPulo * Vector3.up;
+        }
+        if(eixoY.y > 0 &&(controller.collisionFlags & CollisionFlags.Above) !=0)
+        {
+            eixoY = Vector3.zero;
+        }
+
+    }
+}
